d2980ea82da1a8db9cc3ad18183684bff1be2840
cassyay <cmdurkee@gmail.com>
2019-08-22 10:48:20 -0700

    initialize function

--- Ch2 - implement perceptron.py
+++ Ch2 - implement perceptron.py
@@ -1,1 +1,10 @@
 ###implement perceptron using NumPy
+
+import numpy as np 
+
+class Perceptron(object):
+
+	def __init__(self, eta=0.01, n_iter=50, random_state=1):
+		self.eta = eta
+		self.n_iter = n_iter
+		self.random_state = random_state
