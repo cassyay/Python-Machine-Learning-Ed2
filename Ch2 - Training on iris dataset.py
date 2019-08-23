702eb434cb33c2690eefa80cd568f3f7a21c7d67
cassyay <cmdurkee@gmail.com>
2019-08-23 10:00:16 -0700

    convert class labels

--- Ch2 - Training on iris dataset.py
+++ Ch2 - Training on iris dataset.py
@@ -1,3 +1,10 @@
-import pandas as pd 
+import pandas as pd
+import matplotlib.pyplot as plt 
+import numpy as np  
 df = pd.read_csv('https://archive.ics.uci.edu/ml/''machine-learning-databases/iris/iris.data',header=None)
-df.tail()
+print(df.tail())
+
+#convert class labels into integer labels (1 = versicolor; -1 = setosa)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
