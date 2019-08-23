9b1582869c38bd334cac8b179cc3a34dfec690fc
cassyay <cmdurkee@gmail.com>
2019-08-23 14:41:33 -0700

    fixed comment placing

--- Ch2 - Training on iris dataset.py
+++ Ch2 - Training on iris dataset.py
@@ -25,11 +25,12 @@ plt.show()
 
 #train perceptron algorithm on iris data subset
 ppn = Perceptron(eta=0.1, n_iter=10)
+ppn.fit(X, y)
 
 #plot
-ppn.fit(X, y)
 plt.plot(range(1, len(ppn.errors_) + 1),
 	ppn.errors_, marker='o')
 plt.xlabel('Epochs')
 plt.ylabel('Number of updates')
 plt.show()
+
