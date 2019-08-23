7041fab4b00d07a90d756b621ad5128f743835b5
cassyay <cmdurkee@gmail.com>
2019-08-23 10:12:14 -0700

    plot irises

--- Ch2 - Training on iris dataset.py
+++ Ch2 - Training on iris dataset.py
@@ -10,3 +10,13 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 
 #extract sepal and petal length
 X = df.iloc[0:100, [0, 2]].values
+
+#plot
+plt.scatter(X[:50, 0], X[:50, 1],
+	color='red', marker='o', label='setosa')
+plt.scatter(X[50:100, 0], X[50:100, 1],
+	color='blue', marker='x', label='versicolor')
+plt.xlabel('sepal length [cm]')
+plt.ylabel('petal length [cm]')
+plt.legend(loc='upper left')
+plt.show()
